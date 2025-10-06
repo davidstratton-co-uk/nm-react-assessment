61ebabd29418b349ecd6718b83400e6ab2671d8e
David Stratton <me@davidstratton.co.uk>
2025-10-06 08:54:15 +0000

    Added React Added Default Product Image

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import dns from 'node:dns'
+import viteReact from '@vitejs/plugin-react'
 
 export default defineConfig({
+    plugins: [
+        viteReact()
+    ],
     server: {
         allowedHosts: ['.davidstratton.co.uk']        
     },
